7db0c1620191e3986ea294431675926690b2a483
Saurav Shrestha <saurastha@gmail.com>
2024-01-14 23:45:50 +0545

    Add softmax activation

--- activation.py
+++ activation.py
@@ -7,3 +7,16 @@ class ReLU:
 
     def forward(self, x):
         self.output = np.maximum(0, x)
+
+
+class Softmax:
+
+    def __init__(self):
+        self.output = None
+
+    def forward(self, x):
+        exp_values = np.exp(x - np.max(x, axis=1, keepdims=True))
+
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+        self.output = probabilities

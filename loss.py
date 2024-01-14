6ec6f7785c6e8932c1e8ad1e2896cd36b46587b1
Saurav Shrestha <saurastha@gmail.com>
2024-01-15 00:20:19 +0545

    Add categorical_crossentropy loss

---
+++ loss.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+
+        data_loss = np.mean(sample_losses)
+
+        return data_loss
+
+
+class CategoricalCrossEntropy(Loss):
+
+    def forward(self, y_pred, y_true):
+
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            confidences = np.sum(y_pred_clipped * y_true, axis=1)
+        else:
+            raise Exception("Write a exception message.")
+
+        negative_log_likelihoods = -np.log(confidences)
+
+        return negative_log_likelihoods

b787f03f99e226497eb8c1cf8fe648050ac4e6f3
Saurav Shrestha <saurastha@gmail.com>
2024-01-15 16:31:13 +0545

    Add docstring

--- activation.py
+++ activation.py
@@ -2,19 +2,56 @@ import numpy as np
 
 
 class ReLU:
+    """
+        Rectified Linear Unit (ReLU) activation function.
+
+        The ReLU activation function sets all negative values in the input to zero
+        and leaves positive values unchanged.
+
+        Attributes:
+            output (numpy.ndarray): The result of the forward pass.
+
+        Methods:
+            forward(x): Performs the forward pass of the ReLU activation function.
+
+        Example:
+            relu = ReLU()
+            input_data = np.array([-2, -1, 0, 1, 2])
+            relu.forward(input_data)
+            print(relu.output)  # Output: [0, 0, 0, 1, 2]
+    """
     def __init__(self):
         self.output = None
 
-    def forward(self, x):
+    def forward(self, x: np.array) -> np.array:
         self.output = np.maximum(0, x)
 
 
 class Softmax:
+    """
+        Softmax activation function for converting input values into probabilities.
 
+        The Softmax function takes an input array, calculate exponential of each element,
+        and normalizes the values to obtain probabilities that sum to 1.
+
+        Attributes:
+            output (numpy.ndarray): The result of the forward pass representing probabilities.
+
+        Methods:
+            forward(x): Performs the forward pass of the Softmax activation function.
+
+        Example:
+            softmax = Softmax()
+            input_data = np.array([[1.0, 2.0, 3.0], [2.0, 4.0, 6.0]])
+            softmax.forward(input_data)
+            print(softmax.output)
+            # Output: array([[0.09003057, 0.24472847, 0.66524096],
+            #                 [0.01587624, 0.11731043, 0.86681333]])
+    """
     def __init__(self):
         self.output = None
 
-    def forward(self, x):
+    def forward(self, x: np.array) -> np.array:
         exp_values = np.exp(x - np.max(x, axis=1, keepdims=True))
 
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
--- layers.py
+++ layers.py
@@ -2,12 +2,32 @@ import numpy as np
 
 
 class LinearLayer:
-    def __init__(self, in_features, out_features):
+    """
+        Linear layer for neural network computations.
+
+        This class represents a linear layer in a neural network, which performs
+        a linear transformation on the input data using weights and biases.
+
+        Attributes:
+            weights (numpy.ndarray): The weights matrix for the linear transformation.
+            biases (numpy.ndarray): The biases matrix for the linear transformation.
+            output (numpy.ndarray): The result of the forward pass.
+
+        Methods:
+            forward(x): Performs the forward pass of the linear layer.
+
+        Example:
+            linear_layer = LinearLayer(in_features=5, out_features=3)
+            input_data = np.random.randn(10, 5)
+            linear_layer.forward(input_data)
+            print(linear_layer.output)
+    """
+    def __init__(self, in_features: int, out_features: int):
         self.weights = 0.01 * np.random.randn(in_features, out_features)
         self.biases = np.zeros((1, out_features))
         self.output = None
 
-    def forward(self, x):
+    def forward(self, x: np.array) -> np.array:
         self.output = np.dot(x, self.weights) + self.biases
 
 
